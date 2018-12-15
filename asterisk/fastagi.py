88b358f2cca9784d437d9a36594f9018e9e86e1f
Augusto Rocha Nascimento <augusto.nascimento@trestto.com.br>
2018-12-15 13:01:27 -0200

    Inserindo adequação para Python 3

--- asterisk/fastagi.py
+++ asterisk/fastagi.py
@@ -14,31 +14,51 @@ Specification
 """
 
 import sys
-import SocketServer
 import asterisk.agi
+from six import PY3
+if PY3:
+    import socketserver as SocketServer
+else:
+    import SocketServer
 # import pkg_resources
 # PYST_VERSION = pkg_resources.get_distribution("pyst2").version
 
 __verison__ = 0.1
 
-#TODO: Read options from config file.
+# TODO: Read options from config file.
 HOST, PORT = "127.0.0.1", 4573
+
 
 class FastAGI(SocketServer.StreamRequestHandler):
     # Close connections not finished in 5seconds.
     timeout = 5
+
     def handle(self):
         try:
-            agi=asterisk.agi.AGI(stdin=self.rfile, stdout=self.wfile, stderr=sys.stderr)
+            agi = asterisk.agi.AGI(
+                stdin=self.rfile, stdout=self.wfile, stderr=sys.stderr
+                )
             agi.verbose("pyst2: FastAGI on: {}:{}".format(HOST, PORT))
         except TypeError as e:
-            sys.stderr.write('Unable to connect to agi://{} {}\n'.format(self.client_address[0], str(e)))
-        except SocketServer.socket.timeout as e:
-            sys.stderr.write('Timeout receiving data from {}\n'.format(self.client_address))
-        except SocketServer.socket.error as e:
-            sys.stderr.write('Could not open the socket. Is someting else listening on this port?\n')
+            sys.stderr.write(
+                'Unable to connect to agi://{} {}\n'.format(
+                    self.client_address[0], str(e)
+                    )
+                )
+        except SocketServer.socket.timeout:
+            sys.stderr.write(
+                'Timeout receiving data from {}\n'.format(
+                    self.client_address
+                )
+            )
+        except SocketServer.socket.error:
+            sys.stderr.write(
+                """Could not open the socket.
+                Is someting else listening on this port?\n"""
+            )   
         except Exception as e:
-            sys.stderr.write('An unknown error: {}\n'.format(str(e)))
+            sys.stderr.write("""An unknown error: {}\n""".format(str(e)))
+
 
 if __name__ == "__main__":
     # server = SocketServer.TCPServer((HOST, PORT), FastAGI)
